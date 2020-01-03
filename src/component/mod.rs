3d0a1965ab42972e38808a18148d440716ad0bfe
Blaine Price <1wbprice@gmail.com>
2020-01-02 21:13:02 -0500

    7 when asteroids blow up they create smaller asteroids (#22)
    * add small asteroid sprite
    
    * Split big asteroid into little asteroids

--- src/component/mod.rs
+++ src/component/mod.rs
@@ -1,6 +1,10 @@
 mod asteroid;
 mod laser;
 mod player;
+mod small_asteroid;
 mod velocity;
 
-pub use self::{asteroid::Asteroid, laser::Laser, player::Player, velocity::Velocity};
+pub use self::{
+    asteroid::Asteroid, laser::Laser, player::Player, small_asteroid::SmallAsteroid,
+    velocity::Velocity,
+};
--- src/system/lasers_damage_asteroids.rs
+++ src/system/lasers_damage_asteroids.rs
@@ -1,9 +1,13 @@
 use amethyst::{
     core::transform::Transform,
-    ecs::{Entities, Join, ReadStorage, System, WriteStorage},
+    ecs::{Entities, Join, ReadExpect, ReadStorage, System, WriteStorage},
+    renderer::{debug_drawing::DebugLinesComponent, palette::Srgba, SpriteRender},
 };
 
-use crate::component::{Asteroid, Laser, Velocity};
+use crate::{
+    component::{Asteroid, Laser, SmallAsteroid, Velocity},
+    resource::SpriteResource,
+};
 
 pub struct LasersDamageAsteroids;
 
@@ -13,12 +17,32 @@ const ASTEROID_RADIUS: f32 = 36.0;
 impl<'a> System<'a> for LasersDamageAsteroids {
     type SystemData = (
         Entities<'a>,
+        ReadExpect<'a, SpriteResource>,
+        WriteStorage<'a, SpriteRender>,
+        WriteStorage<'a, DebugLinesComponent>,
         ReadStorage<'a, Asteroid>,
+        WriteStorage<'a, SmallAsteroid>,
         ReadStorage<'a, Laser>,
-        ReadStorage<'a, Transform>,
+        WriteStorage<'a, Transform>,
+        WriteStorage<'a, Velocity>,
     );
 
-    fn run(&mut self, (entities, asteroids, lasers, transforms): Self::SystemData) {
+    fn run(
+        &mut self,
+        (
+            entities,
+            sprite_resources,
+            mut sprites,
+            mut debug_lines,
+            asteroids,
+            mut small_asteroids,
+            lasers,
+            mut transforms,
+            mut velocities,
+        ): Self::SystemData,
+    ) {
+        let mut new_small_asteroids: Vec<Transform> = vec![];
+
         for (asteroid_entity, _asteroid, asteroid_local) in
             (&entities, &asteroids, &transforms).join()
         {
@@ -33,10 +57,52 @@ impl<'a> System<'a> for LasersDamageAsteroids {
                 let distance = (dx.powi(2) + dy.powi(2)).sqrt();
 
                 if distance < LASER_RADIUS + ASTEROID_RADIUS {
-                    dbg!("collision!");
                     entities.delete(asteroid_entity).unwrap();
-                    // TODO: spawn two small asteroids
+                    new_small_asteroids.push(asteroid_local.clone());
                 }
+            }
+        }
+
+        for local in new_small_asteroids {
+            let magnitudes: Vec<f32> = vec![0.0, 2.0, -2.0];
+            for i in magnitudes.into_iter() {
+                let local = local.clone();
+                let asteroid_displacement = 75.0;
+                let velocity_x = i.cos() * asteroid_displacement;
+                let velocity_y = i.sin() * asteroid_displacement;
+
+                let pos_x = local.translation().x;
+                let pos_y = local.translation().x;
+
+                let mut debug_component = DebugLinesComponent::new();
+                debug_component.add_circle_2d(
+                    [pos_x, pos_y, 0.0].into(),
+                    6.0,
+                    12,
+                    Srgba::new(0.3, 0.3, 1.0, 1.0),
+                );
+
+                entities
+                    .build_entity()
+                    .with(
+                        SpriteRender {
+                            sprite_sheet: sprite_resources.sprite_sheet.clone(),
+                            sprite_number: 3,
+                        },
+                        &mut sprites,
+                    )
+                    .with(debug_component, &mut debug_lines)
+                    .with(local, &mut transforms)
+                    .with(SmallAsteroid {}, &mut small_asteroids)
+                    .with(
+                        Velocity {
+                            x: velocity_x,
+                            y: velocity_y,
+                            a: 0.,
+                        },
+                        &mut velocities,
+                    )
+                    .build();
             }
         }
     }
