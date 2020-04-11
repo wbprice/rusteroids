24f99538b4070daa16589f66c947d3dcf635e134
Blaine Price <1wbprice@gmail.com>
2020-04-11 12:02:09 -0400

    format

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ mod system;
 
 use crate::system::{
     Collisions, ControlPlayer, DebugBoxes, LasersDamageAsteroids, LasersDamageSmallAsteroids,
-    LasersExpire, MoveObjects, ShipCollidesWithAsteroids
+    LasersExpire, MoveObjects, ShipCollidesWithAsteroids,
 };
 
 fn main() -> amethyst::Result<()> {
@@ -54,7 +54,11 @@ fn main() -> amethyst::Result<()> {
             "lasers_damage_small_asteroids",
             &[],
         )
-        .with(ShipCollidesWithAsteroids, "ships_collide_with_asteroids", &[])
+        .with(
+            ShipCollidesWithAsteroids,
+            "ships_collide_with_asteroids",
+            &[],
+        )
         .with(DebugBoxes, "debug_boxes", &[]);
 
     let mut game = Application::new(resources, state::MyState, game_data)?;
--- src/system/mod.rs
+++ src/system/mod.rs
@@ -11,5 +11,5 @@ pub use self::{
     collisions::Collisions, control_player::ControlPlayer, debug_boxes::DebugBoxes,
     lasers_damage_asteroids::LasersDamageAsteroids,
     lasers_damage_small_asteroids::LasersDamageSmallAsteroids, lasers_expire::LasersExpire,
-    move_objects::MoveObjects, ship_collides_with_asteroid::ShipCollidesWithAsteroids
+    move_objects::MoveObjects, ship_collides_with_asteroid::ShipCollidesWithAsteroids,
 };
